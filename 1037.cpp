085fabaa37f5286ed8ac956804eee0293f9e3f87
Abdul Kader <56482597+abdulkadershohan@users.noreply.github.com>
2020-03-29 20:35:41 +0600

    Create 1037.cpp

---
+++ 1037.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double N;
+    cin>>N;
+    if(N<0.00 || N>100.00)
+    {
+     cout<<"Fora de intervalo"<<endl;
+    }
+    else if(N>25.00 && N<=50.00)
+    {
+        cout<<"Intervalo (25,50]"<<endl;
+    }
+    else if(N>=0.00 && N<=25.00)
+    {
+        cout<<"Intervalo [0,25]"<<endl;
+    }
+    else if(N>50.00 && N<=75.00)
+    {
+         cout<<"Intervalo (50,75]"<<endl;
+    }
+    else if(N>75.00 && N<=100.00)
+    {
+        cout<<"Intervalo (75,100]"<<endl;
+    }
+
+    return 0;
+}
